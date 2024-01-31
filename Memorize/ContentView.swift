cad16d711fbd1112cd7a0973849d3f9685742a90
waikin1216 <waikin1216@gmail.com>
2024-01-31 15:34:33 +0800

    toggleable isFaceUp

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -23,22 +23,26 @@ struct ContentView: View {
 }
 
 struct CardView : View {
-    var isFaceUp : Bool = false
+    @State var isFaceUp : Bool = false
     var body: some View {
-        ZStack(content:{
+        ZStack{
+            let base = RoundedRectangle(cornerRadius: 12)
             if isFaceUp{
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12)
-                    .strokeBorder(lineWidth: 2)
+                base.fill(.white)
+                    
+                base.strokeBorder(lineWidth: 2)
               Text("👻")
                     .font(.largeTitle)
             }else{
-                RoundedRectangle(cornerRadius: 12)
+                base.fill()
                     .foregroundColor(.green)
             }
             
-        })
+        }
+        .onTapGesture {
+//            isFaceUp = !isFaceUp
+            isFaceUp.toggle()
+        }
     }
 }
 
